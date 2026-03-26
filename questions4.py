95e760fa355d1318655d0841459fb716e440146f
angelescancinos <angi.cancinos05@gmail.com>
2026-03-25 21:21:51 -0300

    Cuarto inciso agregado, Varias partidas

--- questions4.py
+++ questions4.py
@@ -11,44 +11,56 @@ words = [
     "lista",
 ]
 
-word = random.choice(words)
-guessed = []
-attempts = 6
-
 print("¡Bienvenido al Ahorcado!")
 print()
-
-
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
-    print(progress)
-
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        break
-
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-
-    letter = input("Ingresá una letra: ")
     
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
-    else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
+# Agregamos bucle para jugar varias partidas
+while len(words) > 0:
 
+    print("¡Adivina la palabra!")
     print()
+    
+    word = random.sample(words,1)[0]
+
+    guessed = []
+    attempts = 6
+
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            break
+
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+
+        letter = input("Ingresá una letra: ")
+        
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
+
+        print()
+    else:
+        print(f"¡Perdiste! La palabra era: {word}")
+
+    # Eliminamos la palabra para no repetir y cumplir la condicion del while
+    words.remove(word)
+
 else:
-    print(f"¡Perdiste! La palabra era: {word}")
+    print("¡Ganaste el juego! Se acabaron las palabras")
